e7bbe884be4b0dcad639f10856ddd9afd4315c23
Walid <walid.elbouchikhi@gmail.com>
2019-04-26 16:29:47 +0200

    Added get picture from camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [Camera]
 })
 export class HomePage {
+  imageData: string;
 
+  constructor(private camera: Camera) { }
+
+  getPicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options)
+      .then((imageData) => {
+        this.imageData = 'data:image/jpeg;base64,' + imageData;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 }
